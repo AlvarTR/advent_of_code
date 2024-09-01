3892d995255838c985106c0a743f276fb63dad72
AlvarTR <alvar.tapiaruiz@gmail.com>
2024-09-02 00:09:00 +0200

    ✅ got first star for day 03

--- 2023/03/gondola.hs
+++ 2023/03/gondola.hs
@@ -1,6 +1,5 @@
-
-import Data.Char ( isDigit )
-import Data.Bifunctor as Bifunctor ( Bifunctor(first, second) )
+import Data.Bifunctor as Bifunctor (Bifunctor (first, second))
+import Data.Char (isDigit)
 import Data.Map qualified as Map
 import Data.Set qualified as Set
 
@@ -10,37 +9,38 @@ rowInfluence = influences "" Set.empty 0
 influences :: String -> Set.Set (Int, Int) -> Int -> Int -> String -> ([Set.Set (Int, Int)], [(Int, Set.Set (Int, Int))])
 influences "" _ _ _ "" = ([], [])
 influences partialNumber partialSet _ _ "" = ([], [(read partialNumber, partialSet)])
-influences "" _ columnNum rowNum ('.':rest) = influences "" Set.empty (columnNum+1) rowNum rest
-influences partialNumber partialSet columnNum rowNum ('.':rest) =
-  Bifunctor.second ((read partialNumber, partialSet):)
-  $ influences "" Set.empty (columnNum+1) rowNum rest
-influences partialNumber partialSet columnNum rowNum (first:rest)
+influences "" _ columnNum rowNum ('.' : rest) = influences "" Set.empty (columnNum + 1) rowNum rest
+influences partialNumber partialSet columnNum rowNum ('.' : rest) =
+  Bifunctor.second ((read partialNumber, partialSet) :) $
+    influences "" Set.empty (columnNum + 1) rowNum rest
+influences partialNumber partialSet columnNum rowNum (first : rest)
   | isDigit first =
-    influences (partialNumber ++ [first])
-    (Set.insert (rowNum, columnNum) partialSet)
-    (columnNum+1) rowNum rest
+      influences
+        (partialNumber ++ [first])
+        (Set.insert (rowNum, columnNum) partialSet)
+        (columnNum + 1)
+        rowNum
+        rest
   | otherwise =
-    Bifunctor.first (influenceGrid:)
-    $ influences partialNumber partialSet columnNum rowNum ('.':rest)
+      Bifunctor.first (influenceGrid :) $
+        influences partialNumber partialSet columnNum rowNum ('.' : rest)
   where
-    influenceGrid = Set.delete (rowNum, columnNum)
-      $ Set.fromList
-      $ zip (concatMap (replicate 3) [rowNum-1 .. rowNum+1])
-      $ concat $ replicate 3 [columnNum-1 .. columnNum+1]
+    influenceGrid =
+      Set.delete (rowNum, columnNum) $
+        Set.fromList $
+          zip (concatMap (replicate 3) [rowNum - 1 .. rowNum + 1]) $
+            concat $
+              replicate 3 [columnNum - 1 .. columnNum + 1]
 
 numbersInfluencedBySymbols :: [[Set.Set (Int, Int)]] -> [[(Int, Set.Set (Int, Int))]] -> [[Int]]
 numbersInfluencedBySymbols _ [] = []
-numbersInfluencedBySymbols [upSetList, sameLineSetList] [first] =
-  let intersectionsWithUp = filter (not . Set.null . Set.intersection (Set.unions upSetList) . snd ) first
-      intersectionsWithMiddle = filter (not . Set.null . Set.intersection (Set.unions sameLineSetList) . snd ) first
-  in [map fst $ intersectionsWithUp ++ intersectionsWithMiddle]
-  -- [Map.keys (Map.filter (not . Set.null . Set.intersection (Set.unions (Set.unions upSetList:[Set.unions sameLineSetList]))) first)]
+numbersInfluencedBySymbols [upSetList, sameLineSetList] [first] = [map fst $ filter (filterOfSetInSecondPlaceOfTuple (upSetList : [sameLineSetList])) first]
+numbersInfluencedBySymbols (upSetList : sameLineSetList : belowSetList : otherSetLists) (first : rest) = map fst intersections : numbersInfluencedBySymbols (sameLineSetList : belowSetList : otherSetLists) rest
+  where
+    intersections = filter (filterOfSetInSecondPlaceOfTuple (upSetList : sameLineSetList : [belowSetList])) first
 
-numbersInfluencedBySymbols (upSetList:sameLineSetList:belowSetList:otherSetLists) (first:rest) =
-  let intersectionsWithUp = filter (not . Set.null . Set.intersection (Set.unions upSetList) . snd ) first
-      intersectionsWithMiddle = filter (not . Set.null . Set.intersection (Set.unions sameLineSetList) . snd ) first
-      intersectionsWithBelow = filter (not . Set.null . Set.intersection (Set.unions belowSetList) . snd ) first
-  in map fst (intersectionsWithUp ++ intersectionsWithMiddle ++ intersectionsWithBelow) : numbersInfluencedBySymbols (sameLineSetList:belowSetList:otherSetLists) rest
+filterOfSetInSecondPlaceOfTuple :: [[Set.Set (Int, Int)]] -> (Int, Set.Set (Int, Int)) -> Bool
+filterOfSetInSecondPlaceOfTuple listOfListsOfSets = not . Set.null . Set.intersection (Set.unions (map Set.unions listOfListsOfSets)) . snd
 
 main :: IO ()
 main = do
@@ -64,7 +64,7 @@ main = do
   -- print "Example parsed:"
   mapM_ print exampleListOfInfluences
 
-  let exampleNumbersWithInfluences = uncurry numbersInfluencedBySymbols $ Bifunctor.first ([]:) $ unzip exampleListOfInfluences
+  let exampleNumbersWithInfluences = uncurry numbersInfluencedBySymbols $ Bifunctor.first ([] :) $ unzip exampleListOfInfluences
   -- print "Example numbers with symbol influence:"
   print exampleNumbersWithInfluences
 
@@ -73,10 +73,10 @@ main = do
 
   contents <- readFile "input.txt"
   let inputListOfInfluences = zipWith rowInfluence [0 ..] $ lines contents
-  let inputNumbersWithInfluences = uncurry numbersInfluencedBySymbols $ Bifunctor.first ([]:) $ unzip inputListOfInfluences
+  let inputNumbersWithInfluences = uncurry numbersInfluencedBySymbols $ Bifunctor.first ([] :) $ unzip inputListOfInfluences
 
   print "Input numbers with symbol influence:"
-  mapM_ print $ zip [0..] inputNumbersWithInfluences
+  mapM_ print $ zip [0 ..] inputNumbersWithInfluences
 
   print "Input sum"
   print $ sum $ map sum inputNumbersWithInfluences
