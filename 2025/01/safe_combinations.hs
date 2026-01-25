6177c91ac237d933f3ed0629e02e3fc0a8fda567
AlvarTR <alvar.tapiaruiz@gmail.com>
2026-01-25 19:29:54 +0100

    🚧 2025-01 second star

--- 2025/01/safe_combinations.hs
+++ 2025/01/safe_combinations.hs
@@ -1,19 +1,35 @@
-import Data.List (isPrefixOf, isSuffixOf)
-import Text.Read (readMaybe)
+import GHC.Natural (Natural, naturalFromInteger)
+import GHC.Num (integerFromNatural)
 
-safe_modulus=100
+safeModulus = 100
 
-turnInstruction :: Int -> Int -> [String] -> (Int, Int)
-turnInstruction state num_zeroes [] = (state, num_zeroes)
-turnInstruction state num_zeroes ((direction:num_turns):rest) = turnInstruction new_state new_num_zeroes rest
+countZeroLandings :: Integer -> Natural -> [String] -> (Integer, Natural)
+countZeroLandings state num_zeroes [] = (state, num_zeroes)
+countZeroLandings state num_zeroes ((direction : num_turns) : rest) = countZeroLandings new_state new_num_zeroes rest
   where
-    new_state = if direction == 'R' 
-                then ((+) state $ read num_turns) `mod` safe_modulus
-                else ((-) state $ read num_turns) `mod` safe_modulus
+    new_state = flip mod safeModulus $ (if direction == 'R' then (+) else (-)) state $ read num_turns
     new_num_zeroes = (+) num_zeroes $ if new_state == 0 then 1 else 0
 
--- listOfTurns :: String -> (Int, Int)
+countZeroPassings :: Integer -> Natural -> [String] -> (Integer, Natural)
+countZeroPassings state num_zeroes [] = (state, num_zeroes)
+countZeroPassings state num_zeroes ((direction : num_turns) : rest) = countZeroPassings new_state new_num_zeroes rest
+  where
+    turns = read num_turns
+    new_unmod_state = (if direction == 'R' then (+) else (-)) state turns
+    (adding_num_zeroes, new_state) = new_unmod_state `divMod` safeModulus
+    new_num_zeroes = (+) num_zeroes $ naturalFromInteger $ (+) adding_num_zeroes $ if (state == 0) && (new_state /= 0) then 1 else 0
 
+debugPassings :: [String] -> [String] -> IO ()
+debugPassings storage [] = do
+  print $ length storage
+  print $ last storage
+  print $ countZeroPassings 50 0 storage
+debugPassings storage (first : rest) = do
+  print $ length storage
+  print $ last storage
+  print $ countZeroPassings 50 0 storage
+  print ""
+  debugPassings (storage ++ [first]) rest
 
 main :: IO ()
 main = do
@@ -29,13 +45,25 @@ main = do
           "R14",
           "L82"
         ]
-    
-  print "Example:"
-  print $ turnInstruction 50 0 $ example
-  print ""
+
+  -- 1st star
+  print "First star example:"
+  print $ countZeroLandings 50 0 example
 
   -- Input text
   contents <- readFile "input.txt"
-  print $ turnInstruction 50 0 $ lines contents
-  -- print ""
-  
+  print "First star input:"
+  print $ countZeroLandings 50 0 $ lines contents
+
+  print ""
+
+  -- 2nd star
+  print "Second star example:"
+  print $ countZeroPassings 50 0 example
+
+  debugPassings [head $ lines contents] $ tail $ lines contents
+
+  print "Second star input:"
+  print $ countZeroPassings 50 0 $ lines contents
+
+-- print ""
