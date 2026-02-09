39fd849f50617bd212715e234f76801c037c8be1
AlvarTR <alvar.tapiaruiz@gmail.com>
2026-02-09 06:50:06 +0100

    🧑‍💻 foundation folder for 2025 advent of code

---
+++ 2025/00/base.hs
@@ -0,0 +1,18 @@
+
+main :: IO ()
+main = do
+  let example =
+        [ "",
+          ""
+        ]
+  -- 1st star
+  print "First star example:"
+  -- Input text
+  contents <- readFile "input.txt"
+
+  print "First star input:"
+  
+  -- 2nd star
+  print "Second star example:"
+
+  print "Second star input:"
