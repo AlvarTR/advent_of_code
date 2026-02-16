d6ed59bf4f71a9f7c697a5bc03b7f054658d8782
AlvarTR <alvar.tapiaruiz@gmail.com>
2026-02-16 21:02:18 +0100

    🧑‍💻 both Haskell project patterns

---
+++ 2025/00-dep/app/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+main :: IO ()
+main = do
+  -- 1st star
+  print "First star example:"
+
+  -- Input text
+  contents <- readFile "input.txt"
+
+  print "First star input:"
+  print ""
+
+  -- 2nd star
+  print "Second star example:"
+
+  print "Second star input:"
