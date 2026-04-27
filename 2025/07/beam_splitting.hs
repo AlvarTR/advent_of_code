59b7ab87c627ce5ed106fb4ee543bf83340590c9
AlvarTR <alvar.tapiaruiz@gmail.com>
2026-04-27 12:33:47 +0200

    ✅ second star for 2025-07

--- 2025/07/beam_splitting.hs
+++ 2025/07/beam_splitting.hs
@@ -1,4 +1,6 @@
-import Data.Set (Set, deleteAt, elemAt, empty, fromList, null, singleton, union)
+import Data.IntMap.Lazy qualified as IntMap
+import Data.Maybe (fromMaybe)
+import Data.Set qualified as Set
 
 firstLineSource :: Int -> String -> Int
 firstLineSource state line
@@ -6,35 +8,43 @@ firstLineSource state line
   | line !! state == 'S' = state
   | otherwise = firstLineSource (state + 1) line
 
-lineSplitterCounter :: Set Int -> Int -> Set Int -> String -> (Int, Set Int)
--- lineSplitterCounter newBeams splitsSoFar oldBeams "" = (splitsSoFasr, newBeams `union` oldBeams)
+lineSplitterCounter :: Set.Set Int -> Int -> Set.Set Int -> String -> (Int, Set.Set Int)
+-- lineSplitterCounter newBeams splitsSoFar oldBeams "" = (splitsSoFasr, newBeams `Set.union` oldBeams)
 lineSplitterCounter newBeams splitsSoFar unprocessedBeams line
-  | Data.Set.null unprocessedBeams = (splitsSoFar, newBeams)
-  | line !! currentBeam == '^' = lineSplitterCounter (union newBeams $ fromList [currentBeam - 1, currentBeam + 1]) (splitsSoFar + 1) lessUnprocessedBeams line
-  | otherwise = lineSplitterCounter (newBeams `union` singleton currentBeam) splitsSoFar lessUnprocessedBeams line
+  | Set.null unprocessedBeams = (splitsSoFar, newBeams)
+  | line !! currentBeam == '^' = lineSplitterCounter (Set.union newBeams $ Set.fromList [currentBeam - 1, currentBeam + 1]) (splitsSoFar + 1) lessUnprocessedBeams line
+  | otherwise = lineSplitterCounter (newBeams `Set.union` Set.singleton currentBeam) splitsSoFar lessUnprocessedBeams line
   where
-    currentBeam = elemAt 0 unprocessedBeams
-    lessUnprocessedBeams = deleteAt 0 unprocessedBeams
+    currentBeam = Set.elemAt 0 unprocessedBeams
+    lessUnprocessedBeams = Set.deleteAt 0 unprocessedBeams
 
-beamSplitter :: Int -> Set Int -> [String] -> Int
+beamSplitter :: Int -> Set.Set Int -> [String] -> Int
 beamSplitter numSplits beams [] = numSplits
 beamSplitter numSplits beams (line : rest) = beamSplitter splitsSoFar newBeams rest
   where
-    (splitsSoFar, newBeams) = lineSplitterCounter empty numSplits beams line
+    (splitsSoFar, newBeams) = lineSplitterCounter Set.empty numSplits beams line
 
 splitterInterface :: [String] -> Int
-splitterInterface (first : rest) = beamSplitter 0 (singleton $ firstLineSource 0 first) rest
+splitterInterface (first : rest) = beamSplitter 0 (Set.singleton $ firstLineSource 0 first) rest
 
-timelineSplitter :: Int -> [String] -> Int
-timelineSplitter beamColumn [] = 1
-timelineSplitter beamColumn (line : rest)
-  | beamEncounter == '.' = timelineSplitter beamColumn rest
-  | beamEncounter == '^' = timelineSplitter (beamColumn - 1) rest + timelineSplitter (beamColumn + 1) rest
+timelineCounter :: IntMap.IntMap Int -> IntMap.IntMap Int -> String -> IntMap.IntMap Int
+-- timelineCounter newBeams splitsSoFar oldBeams "" = (splitsSoFasr, newBeams `IntMap.union` oldBeams)
+timelineCounter newBeams unprocessedBeams line
+  | IntMap.null unprocessedBeams = newBeams
+  | line !! currentBeam == '^' = timelineCounter (IntMap.unionWith (+) newBeams $ IntMap.fromAscList [(currentBeam - 1, beamLegacy), (currentBeam + 1, beamLegacy)]) lessUnprocessedBeams line
+  | otherwise = timelineCounter (IntMap.insertWith (+) currentBeam beamLegacy newBeams) lessUnprocessedBeams line
   where
-    beamEncounter = line !! beamColumn
+    (currentBeam, beamLegacy) = fromMaybe (-1, 0) $ IntMap.lookupMin unprocessedBeams
+    lessUnprocessedBeams = IntMap.deleteMin unprocessedBeams
+
+timelineSplitter :: IntMap.IntMap Int -> [String] -> Int
+timelineSplitter beams [] = sum beams
+timelineSplitter beams (line : rest) = timelineSplitter newBeams rest
+  where
+    newBeams = timelineCounter IntMap.empty beams line
 
 timelineInterface :: [String] -> Int
-timelineInterface (first : rest) = timelineSplitter (firstLineSource 0 first) rest
+timelineInterface (first : rest) = timelineSplitter (IntMap.singleton (firstLineSource 0 first) 1) rest
 
 main :: IO ()
 main = do
